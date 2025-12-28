f80e86d620304919de26fa73364cc1f7e272e7f1
Primož Ajdišek <bigpod@bigpod.si>
2025-12-28 22:40:44 +0000

    small changes

--- internal/providers/aws/provider.go
+++ internal/providers/aws/provider.go
@@ -3,21 +3,15 @@ package aws
 import (
 	"context"
 	"fmt"
-
-	"github.com/NoTuxAllowed/dragon-scheduler/internal/interfaces"
 )
 
 type Provider struct {
 	Config *AwsCloudSpec
 }
 
-func (p *Provider) Provision(ctx context.Context, id string) (*interfaces.Node, error) {
+func (p *Provider) Provision(ctx context.Context, id string) (error) {
 	fmt.Printf("[AWS] Provisioning instance %s with type %s (Spot: %v)\n", id, p.Config.InstanceType, p.Config.Spot)
-	return &interfaces.Node{
-		ID:        id,
-		PrivateIP: "10.0.0.1",
-	},
-	nil
+	return nil
 }
 
 func (p *Provider) Terminate(ctx context.Context, id string) error {
